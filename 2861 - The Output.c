ee71cfee54f7ea9ecc63f51e4439bdcc8774038b
Akib Mahmud <101253688+mahmud-akib@users.noreply.github.com>
2023-07-22 22:27:52 +0600

    Create  2861 - The Output.c

---
+++ 2861 - The Output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[100];
+    int n, i;
+
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+        gets(s);
+        printf("gzuz\n");
+    }
+    return 0;
+}
